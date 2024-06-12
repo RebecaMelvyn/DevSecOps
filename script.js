fb3ad01a3445308eedb37397a0948c3b012ac67e
Melvyn <melvyn.rebeca1@gmail.com>
2024-06-12 11:25:30 +0200

    remove error XSS

--- script.js
+++ script.js
@@ -2,14 +2,4 @@ alert("hello !!");
 
 console.log("yoow");
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Récupère les paramètres de l'URL
-//     const urlParams = new URLSearchParams(window.location.search);
-    
-//     // Récupère le paramètre 'userInput' de l'URL
-//     const userInput = urlParams.get('userInput');
-    
-//     // Insère directement l'input de l'utilisateur dans le DOM
-//     // Ceci est une faille XSS car l'input n'est pas échappé
-//     document.getElementById('output').innerHTML = userInput;
-// });
+
